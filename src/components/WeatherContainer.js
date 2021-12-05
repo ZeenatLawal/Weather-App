9803ebd1a85eb1c3ef86863a176e3514f6b2e6b9
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-12-05 16:48:11 +0100

    Add custom icons for carousel

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,18 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import Carousel from 'react-elastic-carousel';
+import Carousel, { consts } from 'react-elastic-carousel';
+import ArrowCircleLeftIcon from '@mui/icons-material/ArrowCircleLeft';
+import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import Button from '@mui/material/Button';
 import { loadDaily } from '../redux/weatherData/weather';
 import WeatherCard from './WeatherCard';
 import Temperature from './Temperature';
 
-const breakPoints = [
-  { width: 1, itemsToShow: 1 },
-  { width: 768, itemsToShow: 3 },
-];
-
 const WeatherContainer = () => {
   const loading = useSelector((state) => state.weatherReducer.loading);
   const dailyWeather = useSelector((state) => state.weatherReducer.daily);
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 558, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+  ];
+
+  const myArrow = ({ type, onClick, isEdge }) => {
+    const pointer = type === consts.PREV ? <ArrowCircleLeftIcon fontSize="large" /> : <ArrowCircleRightIcon fontSize="large" />;
+    return (
+      <Button onClick={onClick} disabled={isEdge}>
+        {pointer}
+      </Button>
+    );
+  };
 
   const dispatch = useDispatch();
   const [unit, setUnit] = useState('metric');
@@ -34,7 +48,7 @@ const WeatherContainer = () => {
         <>
           <Temperature unit={unit} onChange={onUnitChange} />
           <div className="flex">
-            <Carousel breakPoints={breakPoints}>
+            <Carousel breakPoints={breakPoints} renderArrow={myArrow} pagination={false}>
               {dailyWeather && dailyWeather.map((weather) => (
                 <WeatherCard key={weather.id} weather={weather} />
               ))}
