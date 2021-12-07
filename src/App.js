b67304b7afc72d1ac23dddea606ae8093711ca6a
Zeenat Lawal <zeenatlawal82@gmail.com>
2021-12-07 16:53:56 +0100

    Set custom colors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './styles/App.css';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import WeatherContainer from './components/WeatherContainer';
 import { loadingScreen } from './redux/weatherData/weather';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ffca28',
+    },
+    secondary: {
+      main: '#8ecae6',
+    },
+  },
+});
 
 const App = () => {
   const dispatch = useDispatch();
@@ -12,9 +24,11 @@ const App = () => {
   }, [dispatch]);
 
   return (
-    <div className="App">
-      <WeatherContainer />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <WeatherContainer />
+      </div>
+    </ThemeProvider>
   );
 };
 
